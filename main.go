bc00c1091a6640c84b4e306904c7e496cb11ed5a
Milton Bernhardt <milton.bernhardt@mercadolibre.com>
2021-12-22 18:23:58 -0300

    ejercicio clase 1 parte 2 terminados se crearon request para probar los endpoints

--- main.go
+++ main.go
@@ -1,48 +1,13 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"github.com/gin-gonic/gin"
-	"os"
 )
 
-func GetAll(ctx *gin.Context) {
-	var users []User
-	usersJson, err := os.ReadFile("./users.json")
-
-	if err != nil {
-		ctx.String(500, "error: no se pudo leer el archivo")
-	}
-
-	err = json.Unmarshal(usersJson, &users)
-
-	if err != nil {
-		ctx.String(500, "error: no se pudo descifrar el archivo")
-	}
-
-	fmt.Println(users)
-
-	ctx.JSON(200, users)
-}
-
-func Greeting(ctx *gin.Context) {
-	ctx.String(200, "Hola Milton!")
-
-}
 func main() {
 	r := gin.Default() // add middleware (Logger & Recovery)
-	r.GET("/greeting", Greeting)
-	r.GET("/users", GetAll)
+	r.GET("/hello", Hello)
+	r.GET("/users", GetUsers)
+	r.GET("/users/:id", GetUserById)
 	_ = r.Run() // listen and serve on 0.0.0.0:8080 | "localhost:8080"
-}
-
-type User struct {
-	Id            string
-	Nombre        string `json:"nombre"`
-	Apellido      string `json:"apellido"`
-	Email         string `json:"email"`
-	Altura        int    `json:"edad"`
-	Activo        bool   `json:"activo"`
-	FechaCreacion string `json:"fecha_creacion"`
 }
