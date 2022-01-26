7ba1341c89d8a1fc72215aa45fe6d4defd4d39b1
Milton Bernhardt <milton.bernhardt@mercadolibre.com>
2022-01-26 14:39:41 -0300

    script db modified

--- db.sql
+++ db.sql
@@ -1,17 +1,25 @@
 CREATE DATABASE IF NOT EXISTS `storage` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+
 USE `storage`;
-CREATE TABLE `products`
+
+CREATE TABLE `users`
 (
-    `id`    int(11)     NOT NULL,
-    `name`  varchar(60) NOT NULL,
-    `type`  varchar(60) NOT NULL,
-    `count` int(11)     NOT NULL,
-    `price` float       NOT NULL
+    `id`           int(11)     NOT NULL,
+    `firstname`    varchar(30) NOT NULL,
+    `lastname`     varchar(30) NOT NULL,
+    `email`        varchar(60) NOT NULL,
+    `age`          int(3)      NOT NULL,
+    `height`       int(3)      NOT NULL,
+    `active`       bool        NOT NULL,
+    `created_date` varchar(60) NOT NULL
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
-ALTER TABLE `products`
+
+ALTER TABLE `users`
     ADD PRIMARY KEY (`id`);
-ALTER TABLE `products`
+
+ALTER TABLE `users`
     MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,
     AUTO_INCREMENT = 4;
+
 COMMIT;
